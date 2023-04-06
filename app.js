ca55ffcfdab897d9cd5b6b9f7a785836f6cec2d8
Miss Maryam <maryamdev.lop@gmail.com>
2023-04-06 21:45:04 +0500

    practice class2 smit

--- app.js
+++ app.js
@@ -1,5 +1,62 @@
-let data = "HELLO";
-let data1 = "STUDENTS";
- 
-let data3 = data + data1;
-console.log(data3);
+alert("java script 1 ");
+
+// let data = "5";
+// let data1 = 7;
+// console.log(data + data1);
+
+// let data = "5";
+// let data1 = 7;
+// console.log(+data + data1);
+
+// let data = "5";
+// let data1 = 7;
+// console.log(data + data1);
+
+// let data = "5";
+// let data1 = 7;
+// console.log(data - data1);
+
+// let data = "5";
+// let data1 = 7;
+// console.log(data * data1);
+
+// let data = "5";
+// let data1 = 7;
+// console.log(data / data1);
+
+// module gives reminder
+// let data = 20;
+// let data1 = 7;
+// console.log(data % data1);
+
+// let userFavNum = prompt("how many birds you like most?");
+// console.log(userFavNum);
+
+// let userName = prompt("what is your Name?");
+// console.log(userName);
+
+// let userName = prompt("what is your Name?");
+// console.log("welcome back " + userName);
+
+// let randomNumber = Math.random(); 
+// console.log(randomNumber);
+
+// let randomNumber = Math.random(); 
+// randomNumber =  randomNumber * 10
+// console.log(randomNumber);
+
+// let randomNumber = Math.random(); 
+// // randomNumber =  randomNumber * 10;
+// randomNumber *= 10 * 4; // short hand
+// let roundedNum = Math.round(randomNumber);
+// console.log(roundedNum);
+
+// let Num = 5 + 5 * 5;
+// console.log(Num);
+
+// let Num = 5 ** 3 ;
+// console.log(Num);
+
+
+
+
