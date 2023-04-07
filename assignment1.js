97d7deeafa23673ad2b63a8ccc24412110268308
Miss Maryam <maryamdev.lop@gmail.com>
2023-04-07 18:41:08 +0500

    solution question no 4

--- assignment1.js
+++ assignment1.js
@@ -44,3 +44,20 @@
 // else{
 //     console.log("the largest no is Number 5 " + num5);
 // }
+
+// qno 4
+
+for (let a=0; a<=15; a++) {
+    if (a === 0) {
+            console.log(a +  " is even");
+    }
+    else if (a % 2 === 0) {
+            console.log(a + " is even");   
+    }
+    else {
+            console.log(a + " is odd");
+    }
+}
+
+// qno 5
+
