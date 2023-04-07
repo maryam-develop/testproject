cb3112d05c75b4d3ef982ee37be1e8606f03ccb5
Miss Maryam <maryamdev.lop@gmail.com>
2023-04-07 18:32:11 +0500

    assignments

--- assignment1.js
+++ assignment1.js
@@ -1,2 +1,46 @@
 // question no1 
 
+// let num1 = +prompt(" Enter a Number");
+// let num2 = +prompt(" Enter a Number");
+
+// if(num1>num2){
+//     console.log("the larger no is" + num1);
+// }
+// else{
+//     console.log("the larger no is" + num2);
+// }
+
+// question no2
+
+// let num3 = +prompt(" Enter a Number");
+//  if (num3 >  0) {
+//     alert("the sign is + ");
+//  } else {
+//     alert("the sign is - ");
+//  }
+
+
+// question no3
+
+// let num1 = +prompt(" Enter a Number 1");
+// let num2 = +prompt(" Enter a Number 2");
+// let num3 = +prompt(" Enter a Number 3");
+// let num4 = +prompt(" Enter a Number 4");
+// let num5 = +prompt(" Enter a Number 5");
+
+// if(num1 >= num2 && num1 >= num3 && num1 >= num4 && num1 >= num5) {
+//     console.log("the largest no is Number 1  " + num1);
+// }
+// else if (num2 >= num1 && num2 >= num3 && num2 >= num4 && num2 >= num5) {
+//     console.log("the largest no is Number 2 " + num2);
+// }
+// else if (num3 >= num1 && num3 >= num2 && num3 >= num4 && num3 >= num5) {
+//     console.log("the largest no is Number 3 " + num3);
+
+// }
+// else if (num4 >= num1 && num4 >= num2 && num4 >= num3 && num4 >= num5) {
+//     console.log("the largest no is Number 4 " + num4);
+// }
+// else{
+//     console.log("the largest no is Number 5 " + num5);
+// }
