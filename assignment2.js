b764f60b33333025e9ba515463cad4413fcc812e
maryam-develop <maryamdev.lop@gmail.com>
2023-04-12 02:38:14 +0500

    working on questions

--- assignment2.js
+++ assignment2.js
@@ -1,10 +1,65 @@
 // Task 1
 
-function clos(num1){
-        return function(num2){
-            console.log(num1 + num2);
-        }
-    }
+// function clos(num1){
+//         return function(num2){
+//             console.log(num1 + num2);
+//         }
+//     }
 
-    let closure1 = clos(9);
-    closure1(5);
+//     let closure1 = clos(9);
+//     closure1(5);
+
+// task 2 
+
+// let A = [1,4,2,3,6,7,9,3]
+
+// function recSearch(A,value){
+// if(A.length === 0){
+//     return false;
+// }
+// else if(A[0]===value){
+//        return true
+//     }
+// else{
+//         return recSearch(A.slice(5), value);
+//     }
+// };
+
+// console.log(recSearch(A,2));
+
+
+
+// // Task 3
+
+//   function addPara() {
+//     let adPara = prompt("Welcome back");
+//     const newParagraph = document.createElement('p');
+//     newParagraph.innerHTML = adPara; 
+//     document.body.appendChild(newParagraph); 
+//   }
+
+//Task 4
+
+// function addList() {
+//     let para = prompt("Complete you List Item");
+//     const fetching = document.getElementsByTagName('ul');
+//     const creating = document.createElement('li');
+//     creating.innerHTML = para; 
+//     document.body.appendChild(creating); 
+//  }
+
+ 
+
+
+//task 5
+
+// function changeColor(sub,color){
+// sub.style.backgroundColor = color;
+// }
+// let sub = document.getElementById("body");
+// changeColor(sub,"lime");
+
+// Task 6
+
+
+let storeData = 
